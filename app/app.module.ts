d030d3e6466240367beaf9b0fe67d712151d1ee6
fabioa <fab.arciniegas@gmail.com>
2016-08-10 16:31:39 +0800

    up to static dashboard

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,14 +4,21 @@ import { FormsModule }    from '@angular/forms';
 import { AppComponent }   from './app.component';
 import { HeroesComponent }  from './heroes.component';
 import { HeroService }  from './hero.service';
+import { HeroDetailComponent } from './hero-detail.component';
+import { routing }        from './app.routing';
+import { DashboardComponent } from './dashboard.component';
+
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    routing
   ],
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroDetailComponent,
+    HeroesComponent,
+    DashboardComponent
   ],
   providers: [
     HeroService
--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
 import { OnInit } from '@angular/core';
+import { HeroDetailComponent } from './hero-detail.component';
 
 @Component({
   selector: 'my-heroes',
   template:`
-  <h1>{{title}}</h1>
   <h2>My Heroes</h2>
   <ul class="heroes">
     <li *ngFor="let hero of heroes" [class.selected]="hero === selectedHero" (click)="onSelect(hero)">    
