efd351f3d565c274fa498e064e2b92eb06c6733a
fabioa <fab.arciniegas@gmail.com>
2016-08-10 16:08:37 +0800

    routing, not working

--- README.md
+++ README.md
@@ -8,3 +8,5 @@ This repository is organized by tags, corresponding to each lesson:
  - v1.1 Two way binding for a details editor (https://angular.io/docs/ts/latest/tutorial/toh-pt1.html)
  - v1.2 Master/Detail list (https://angular.io/docs/ts/latest/tutorial/toh-pt2.html)
  - v1.3 Modularizing version 1.2 into more than one component (https://angular.io/docs/ts/latest/tutorial/toh-pt3.html)
+ - v1.4 Services and promises (https://angular.io/docs/ts/latest/tutorial/toh-pt4.html#!#slow)
+ - v1.5 Routing (https://angular.io/docs/ts/latest/tutorial/toh-pt5.html)
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,13 +1,22 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms';
-import { HeroDetailComponent } from './hero-detail.component';
-
-import { AppComponent }  from './app.component';
-
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
+import { AppComponent }   from './app.component';
+import { HeroesComponent }  from './heroes.component';
+import { HeroService }  from './hero.service';
 @NgModule({
-  imports:      [ BrowserModule,FormsModule ],
-  declarations: [ AppComponent, HeroDetailComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  declarations: [
+    AppComponent,
+    HeroesComponent
+  ],
+  providers: [
+    HeroService
+  ],
+  bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {
+}
